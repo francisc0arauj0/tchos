0000bf11d673f6eda4f3c4fbc7bae6a4f027aa4b
Francisco <franciscocoelhoaraujo492000@gmail.com>
2025-09-06 10:08:14 +0100

    update build

--- build/config.mk
+++ build/config.mk
@@ -2,7 +2,7 @@ C_COMPILER = gcc
 C_FLAGS = -mcmodel=large -ffreestanding -O2 -Wall -Werror -Wextra
 
 LD = ld
-LD_FLAGS = -nostdlib -T ../tchux/linker.ld
+LD_FLAGS = -nostdlib -T ../kernel/linker.ld
 
 XORRISO_FLAGS = -R -r -J -hfsplus -apm-block-size 2048 --efi-boot boot/limine/limine-uefi-cd.bin -efi-boot-part --efi-boot-image --protective-msdos-label
 QEMU_FLAGS = -M q35 -m 2G -monitor stdio -d int -M smm=off
